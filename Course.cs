e8e2d5fc60251429ef01405537bd00364cf2b7a0
IMDAD <64931177+imdadhaq@users.noreply.github.com>
2020-11-19 23:19:12 +0600

    MD IMDADUL HAQUE

---
+++ Course.cs
@@ -0,0 +1,22 @@
+﻿namespace Project
+{
+    public class Course
+    {
+        public teacher Teacher;
+         private string cname;
+         private string cnum;
+        
+                public string Cname
+                {
+                    get => cname;
+                    set => cname = value;
+                }
+                
+        
+                public string Cnum
+                {
+                    get => cnum; 
+                    set => cnum = value;
+                }
+    }
+}
---
+++ De.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Project
+{
+    public class De
+    {
+       
+
+        
+    }
+}
+
+
